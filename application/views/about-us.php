ca49d57991032035b2114e03a2b3a37903274fb0
vaio <vaio@192.168.128.8>
2014-10-11 17:22:19 +0530

    -changed about us

--- application/views/about-us.php
+++ application/views/about-us.php
@@ -35,10 +35,12 @@
             </div>
             <div class="collapse navbar-collapse">
                 <ul class="nav navbar-nav navbar-right">
+
                     <li><a href="<?php echo site_url();?>">Home</a></li>
                     <li class="active"><a href="<?php echo site_url();?>/loadViewsController/loadAboutus">About Us</a></li>
                     <li><a href="<?php echo site_url();?>/loadViewsController/loadServices">Services</a></li>
                     <li><a href="<?php echo site_url();?>/loadViewsController/loadPortfolio">Portfolio</a></li>
+
 <!--                    <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown">Pages <i class="icon-angle-down"></i></a>
                         <ul class="dropdown-menu">
@@ -54,6 +56,7 @@
                         </ul>
                     </li>
                     <li><a href="blog.html">Blog</a></li> -->
+
                     <li><a href="<?php echo site_url();?>/loadViewsController/loadContactus">Contact</a></li>
                 </ul>
             </div>
@@ -65,11 +68,11 @@
             <div class="row">
                 <div class="col-sm-6">
                     <h1>About Us</h1>
-                    <p>Pellentesque habitant morbi tristique senectus et netus et malesuada</p>
+<!--                    <p></p>-->
                 </div>
                 <div class="col-sm-6">
                     <ul class="breadcrumb pull-right">
-                        <li><a href="index.html">Home</a></li>
+                        <li><a href="index.php">Home</a></li>
                         <li class="active">About Us</li>
                     </ul>
                 </div>
@@ -80,77 +83,87 @@
     <section id="about-us" class="container">
         <div class="row">
             <div class="col-sm-6">
-                <h2>What we are</h2>
-                <p>Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor a ornare odio. Sed non  mauris vitae erat consequat auctor eu in elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris in erat justo. Nullam ac urna eu felis dapibus condimentum sit amet a augue. Sed non neque elit. Sed ut imperdiet nisi. Proin condimentum fermentum nunc. Etiam pharetra, erat sed fermentum feugiat, velit mauris egestas quam, ut aliquam massa nisl quis neque. Suspendisse in orci enim.</p>
+                <h2>What we do?</h2>
+                <p>iTechRoute is a Software Training and Development Company providing services in Product Development, Application Development, Software Development, Web Development, Academic IT Projects, Training services to IT and Non-IT Clients.</p>
+                <h2>What we are?</h2>
+                <p>We are a team of young dynamic IT professionals; with our latest skill, we excel in making software products development and training is at par excellence with affordable cost.</p>
+                <h2>Why Us?</h2>
+                <p>We at iTechRoute believe in full custom and tailor made software products that suits individual organization lending its business to grow with an IT twist.</p>
+                <p>Research work done over 7+ years on IEEE titles and projects developed under data mining, machine learning, image processing, security, mobile applications and much more…</p>
+                <p>Our Team is experienced, young, dynamic under Veteran Leadership which specializes in understanding IT needs of an organization in all directions and versatile orientation.</p>
             </div><!--/.col-sm-6-->
             <div class="col-sm-6">
-                <h2>Our Skills</h2>
+                <h2>We expertise in</h2>
                 <div>
                     <div class="progress">
-                        <div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100" style="width: 90%;">
-                            <span>HTML/CSS</span>
+                        <div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100" style="width: 80%;">
+                            <span>Web Development</span>
                         </div>
                     </div>
                     <div class="progress">
-                        <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width: 70%;">
-                            <span>Wordpress</span>
+                        <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width: 85%;">
+                            <span>IT Consultancy</span>
                         </div>
                     </div>
                     <div class="progress">
-                        <div class="progress-bar" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%;">
-                            <span>Joomla</span>
+                        <div class="progress-bar" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 75%;">
+                            <span>Software Development</span>
                         </div>
                     </div>
                     <div class="progress">
-                        <div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100" style="width: 55%;">
-                            <span>Drupal</span>
+                        <div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100" style="width: 92%;">
+                            <span>Academic Year Projects</span>
                         </div>
                     </div>
-                    <div class="progress">
+<!--                    <div class="progress">
                         <div class="progress-bar progress-bar-warning" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%;">
                             <span>PHP</span>
                         </div>
-                    </div>
+                    </div>-->
                 </div>
             </div><!--/.col-sm-6-->
         </div><!--/.row-->
 
         <div class="gap"></div>
         <h1 class="center">Meet the Team</h1>
-        <p class="lead center">Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+        <p class="lead center"></p>
         <div class="gap"></div>
 
         <div id="meet-the-team" class="row">
             <div class="col-md-3 col-xs-6">
                 <div class="center">
-                    <p><img class="img-responsive img-thumbnail img-circle" src="<?php echo base_url();?>application/views/images/team-member.jpg" alt="" ></p>
-                    <h5>David J. Robbins<small class="designation muted">Senior Vice President</small></h5>
-                    <p>Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor.</p>
+
+<!--                    <p><img class="img-responsive img-thumbnail img-circle" src="<?php echo base_url();?>application/views/images/team-member.jpg" alt="" ></p>-->
+                    <h5>Mrs. Ranjana Wankhade<small class="designation muted">Co-Founder</small></h5>
+                    <p>Dreams have only one owner at a time.</p>
                     <a class="btn btn-social btn-facebook" href="#"><i class="icon-facebook"></i></a> <a class="btn btn-social btn-google-plus" href="#"><i class="icon-google-plus"></i></a> <a class="btn btn-social btn-twitter" href="#"><i class="icon-twitter"></i></a> <a class="btn btn-social btn-linkedin" href="#"><i class="icon-linkedin"></i></a>
                 </div>
             </div>
 
             <div class="col-md-3 col-xs-6">
                 <div class="center">
-                    <p><img class="img-responsive img-thumbnail img-circle" src="<?php echo base_url();?>application/views/images/team-member.jpg" alt="" ></p>
-                    <h5>David J. Robbins<small class="designation muted">Senior Vice President</small></h5>
-                    <p>Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor.</p>
+
+<!--                    <p><img class="img-responsive img-thumbnail img-circle" src="<?php echo base_url();?>application/views/images/team-member.jpg" alt="" ></p>-->
+                    <h5>Mr. Deepak Bhaktiani<small class="designation muted">Operations Manager</small></h5>
+                    <p>Do celebrate milestones as you prepare for the road ahead.</p>
                     <a class="btn btn-social btn-facebook" href="#"><i class="icon-facebook"></i></a> <a class="btn btn-social btn-google-plus" href="#"><i class="icon-google-plus"></i></a> <a class="btn btn-social btn-twitter" href="#"><i class="icon-twitter"></i></a> <a class="btn btn-social btn-linkedin" href="#"><i class="icon-linkedin"></i></a>
                 </div>
             </div>        
             <div class="col-md-3 col-xs-6">
                 <div class="center">
-                    <p><img class="img-responsive img-thumbnail img-circle" src="<?php echo base_url();?>application/views/images/team-member.jpg" alt="" ></p>
-                    <h5>David J. Robbins<small class="designation muted">Senior Vice President</small></h5>
-                    <p>Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor.</p>
+<!--                    <p><img class="img-responsive img-thumbnail img-circle" src="<?php echo base_url();?>application/views/images/team-member.jpg" alt="" ></p>-->
+                    <h5>Mr. Piyush Chandekar<small class="designation muted">Project Manager</small></h5>
+                    <p>As long as you’re going to be thinking anyway, think big.</p>
+
                     <a class="btn btn-social btn-facebook" href="#"><i class="icon-facebook"></i></a> <a class="btn btn-social btn-google-plus" href="#"><i class="icon-google-plus"></i></a> <a class="btn btn-social btn-twitter" href="#"><i class="icon-twitter"></i></a> <a class="btn btn-social btn-linkedin" href="#"><i class="icon-linkedin"></i></a>
                 </div>
             </div>        
             <div class="col-md-3 col-xs-6">
                 <div class="center">
-                    <p><img class="img-responsive img-thumbnail img-circle" src="<?php echo base_url();?>application/views/images/team-member.jpg" alt="" ></p>
-                    <h5>David J. Robbins<small class="designation muted">Senior Vice President</small></h5>
-                    <p>Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor.</p>
+                    <!--<p><img class="img-responsive img-thumbnail img-circle" src="<?php echo base_url();?>application/views/images/team-member.jpg" alt="" ></p>-->
+                    <h5>Mr. Laxman Maskar<small class="designation muted">Project Manager</small></h5>
+                    <p>Always deliver more than expected.</p>
+                    
                     <a class="btn btn-social btn-facebook" href="#"><i class="icon-facebook"></i></a> <a class="btn btn-social btn-google-plus" href="#"><i class="icon-google-plus"></i></a> <a class="btn btn-social btn-twitter" href="#"><i class="icon-twitter"></i></a> <a class="btn btn-social btn-linkedin" href="#"><i class="icon-linkedin"></i></a>
                 </div>
             </div>
@@ -162,8 +175,8 @@
             <div class="row">
                 <div class="col-md-3 col-sm-6">
                     <h4>About Us</h4>
-                    <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante.</p>
-                    <p>Pellentesque habitant morbi tristique senectus.</p>
+                    <p>@iTechRoute we came in to make people find their way in the world of Information Technology…</p>
+                    <p>We at iTechRoute have grown up to build your Business online; in fact we connect your Business to the world!! iTechRoute not only publishes you online but also make business level applications for your company or an organization!</p>
                 </div><!--/.col-md-3-->
 
                 <div class="col-md-3 col-sm-6">
@@ -263,3 +276,4 @@
     <script src="<?php echo base_url();?>application/views/js/main.js"></script>
 </body>
 </html>
+
